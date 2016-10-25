ac752524b5d22b9195dc4410c5064bf08a575966
David Denicolò <ddenicolo@snowflake.ch>
2016-10-25 12:41:39 +0200

    [ch03] fix small bugs

--- chapters/ch03/express_03bis/express_03bis.js
+++ chapters/ch03/express_03bis/express_03bis.js
@@ -1,7 +1,7 @@
 /**
  * Created by david on 24/10/16.
  */
-
+"use strict";
 // setup express
 var express = require("express");
 var app = express();
@@ -13,25 +13,28 @@ var handlebars = require("express3-handlebars") .create({
     defaultLayout: "main",
     extname: ".hbs" // this define the extension for layouts/main.hbs
 });
+
 app.engine("hbs", handlebars.engine); // this define hbs as handlebars engine
 app.set("view engine", "hbs"); // this say to use hbs as view engine
 
 
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", function(req, res) { res.render("home");
+app.get("/", function(req, res) {
+    res.render("home");
 });
 app.get("/about", function(req, res) {
     res.render("about");
 });
 
 // custom 404 page
-app.use(function(req, res){ res.type("text/plain");
+app.use(function(req, res){
+    res.type("text/plain");
     res.status(404);
     res.render("404");
 });
 // custom 500 page
-app.use(function(err, req, res, next){ console.error(err.stack);
+app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500);
     res.render("500");
